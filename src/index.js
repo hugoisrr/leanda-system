71606b6d0f349bb2beeaa09c2a2eeec74c2c0ff2
Hugo Ramirez <hugo@pop-os.localdomain>
2020-01-29 19:03:45 +0100

    SocketIO Setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 import app from './app';
 import connectDB from './config/database';
+import socketIO from './config/socketio';
 // import connectUniPi from './config/unipi';
 
 dotenv.config();
@@ -11,9 +12,18 @@ async function main() {
   connectDB();
   // connectUniPi();
   const port = app.get('port');
-  await app.listen(port, () =>
+  const server = await app.listen(port, () =>
     console.log('\x1b[36m%s\x1b[0m', `Server started on port ${port}`)
   );
+
+  // Setup Socket io connection
+  const io = socketIO.init(server);
+  io.on('connection', clientSocket => {
+    console.log(
+      '\x1b[40m%s\x1b[0m',
+      `Client connected to Server ${clientSocket.id}`
+    );
+  });
 }
 
 main();
